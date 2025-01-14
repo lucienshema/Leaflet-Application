090152417a420e1a05be2d4f5bec36e24c65c718
lucienshema <lshema@tns.org>
2025-01-14 09:25:00 +0200

    added controls icons and improved the map

--- app/src/main/java/org/technoserve/leafletapplication/MainActivity.kt
+++ app/src/main/java/org/technoserve/leafletapplication/MainActivity.kt
@@ -8,8 +8,6 @@ import android.net.NetworkCapabilities
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.view.ViewGroup
 import android.webkit.*
 import androidx.activity.ComponentActivity
@@ -26,10 +24,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.viewinterop.AndroidView
-import java.io.BufferedReader
 import java.io.File
-import java.io.IOException
-import java.io.InputStreamReader
 import java.net.URLConnection
 
 var loadURL = "file:///android_asset/leaflet_map.html"
@@ -48,114 +43,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-
-//@SuppressLint("SetJavaScriptEnabled")
-//@Composable
-//fun WebViewPage(url: String) {
-//    val context = LocalContext.current
-//    //.................................................
-//    // Compose WebView Part 9 | Removes or Stop Ad in web
-//    val adServers = StringBuilder()
-//    var line: String? = ""
-//    val inputStream = context.resources.openRawResource(R.raw.adblockserverlist)
-//    val br = BufferedReader(InputStreamReader(inputStream))
-//    try {
-//        while (br.readLine().also { line = it } != null) {
-//            adServers.append(line)
-//            adServers.append("\n")
-//        }
-//    } catch (e: IOException) {
-//        e.printStackTrace()
-//    }
-//
-//    var backEnabled by remember { mutableStateOf(false) }
-//    var webView: WebView? = null
-//
-//    AndroidView(
-//        factory = {
-//            WebView(it).apply {
-//                layoutParams = ViewGroup.LayoutParams(
-//                    ViewGroup.LayoutParams.MATCH_PARENT,
-//                    ViewGroup.LayoutParams.MATCH_PARENT
-//                )
-//
-//                // Enable JavaScript and required settings
-//                settings.apply {
-//                    javaScriptEnabled = true
-//                    domStorageEnabled = true
-//                    setGeolocationEnabled(true)
-//                    builtInZoomControls = true
-//                    displayZoomControls = false
-//                    allowFileAccess = true
-//                    allowContentAccess = true
-//                    loadWithOverviewMode = true
-//                    useWideViewPort = true
-//
-//                    // Add these new settings
-//                    cacheMode = WebSettings.LOAD_NO_CACHE
-//                    setRenderPriority(WebSettings.RenderPriority.HIGH)
-//                    setEnableSmoothTransition(true)
-//
-//                    // Enable hardware acceleration
-//                    setLayerType(View.LAYER_TYPE_HARDWARE, null)
-//                }
-//
-//                webChromeClient = object : WebChromeClient() {
-//                    override fun onGeolocationPermissionsShowPrompt(
-//                        origin: String,
-//                        callback: GeolocationPermissions.Callback
-//                    ) {
-//                        callback.invoke(origin, true, false)
-//                    }
-//                }
-//
-//                webViewClient = object : WebViewClient() {
-//                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
-//                        super.onPageStarted(view, url, favicon)
-//                    }
-//
-//                    override fun onPageFinished(view: WebView?, url: String?) {
-//                        super.onPageFinished(view, url)
-//                        // Force map refresh after page load
-//                        view?.evaluateJavascript("if(map){map.invalidateSize(true);}", null)
-//                    }
-//                }
-//
-//                loadUrl(url)
-//                webView = this
-//            }
-//        },
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .semantics { contentDescription = "Web View" },
-//        update = { webView ->
-//            webView.evaluateJavascript("if(map){map.invalidateSize(true);}", null)
-//        }
-//    )
-//    BackHandler(enabled = backEnabled) {
-//        removeElement(webView!!)
-//        webView?.goBack()
-//    }
-//
-//}
-
-//fun removeElement(webView: WebView) {
-//
-//    // hide element by id
-//    webView.loadUrl("javascript:(function() { document.getElementById('blog-pager').style.display='none';})()");
-//
-//    // we can also hide class name
-//    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[0].style.display='none';})()")
-//    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[1].style.display='none';})()")
-//    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[2].style.display='none';})()")
-//    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[3].style.display='none';})()")
-//    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[4].style.display='none';})()")
-//    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[5].style.display='none';})()")
-//    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[6].style.display='none';})()")
-//}
-
 
 /**
  * Implementation of caching mechanism for performance
